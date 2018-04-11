565ff3a68887f4d8ad267f6b1084927ba225afc2
Evan Knebel <s179060@students.aie.edu.au>
2018-04-10 17:54:52 -0700

    Shmuuuup

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+
+        Vector3 verthorz = new Vector3(horz, 0, vert);
+
+        rb.AddForce( verthorz * speed);
+    }
+
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    //public GameObject target;
+    public GameObject projectile;
+    public GameObject me;
+
+    public float speed;
+    public float shootInterval;
+    float timer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void spawnThing()
+    {
+        GameObject spawnedThing = Instantiate(projectile);
+        Destroy(spawnedThing, shootInterval + 2);
+        spawnedThing.transform.position = transform.position + transform.forward * 2;
+        
+        Vector3 shootDir = (spawnedThing.transform.position - me.transform.position).normalized;
+        spawnedThing.GetComponent<Rigidbody>().velocity = shootDir.normalized * speed;
+        spawnedThing.transform.forward = shootDir.normalized;
+
+        //me.transform.forward = shootDir.normalized;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            spawnThing();
+            timer = shootInterval;
+        }
+    }
+}
